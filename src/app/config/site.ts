0f7f81f5d0a8fcf4a74680887a74847cf43ff687
pranav <pranav.bsbhat@gmail.com>
2024-10-07 12:16:32 +0530

    edited

--- src/app/config/site.ts
+++ src/app/config/site.ts
@@ -17,8 +17,7 @@ export const siteConfig = {
   name: "Skateshop",
   description:
     "An open source e-commerce skateshop build with everything new in Next.js.",
-  url: "https://skateshop.sadmn.com",
-  ogImage: "https://skateshop.sadmn.com/opengraph-image.png",
+  url: "/",
   links,
   mainNav: [
     {
