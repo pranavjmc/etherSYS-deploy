080067d623dc939271f990f22ffe6c756f5874ca
pranav <pranav.bsbhat@gmail.com>
2024-10-15 12:33:55 +0530

    changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import { LobbySkeleton } from "./skeleton/HeroSection";
 import { SiteHeader } from "@/skate-components/layouts/site-header";
+import PreFooter from "./skeleton/PreFooter";
 
 export default function Home() {
   return (
     <main>
       <SiteHeader />
       <LobbySkeleton />
+      <PreFooter/>
     </main>
   );
 }
